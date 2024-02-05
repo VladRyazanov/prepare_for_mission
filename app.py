bbd51e20cc9faf10d6450ba6d5e94e52132aa1c4
vlad <v1adryazanov@yandex.com>
2024-02-05 19:38:42 +0300

    версия

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def index(title):
     return render_template("base.html", page_title=title)
 
 
+@app.route("/training/<prof>")
+def training(prof):
+    if "строитель" in prof or "инженер" in prof:
+        return render_template("training.html", prof="инженер")
+    return render_template("training.html")
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
