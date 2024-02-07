3077a7c2a2094d18c73d7f27a54caf3d176ceafb
vlad <v1adryazanov@yandex.com>
2024-02-07 20:32:32 +0300

    версия 4

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
@@ -20,6 +19,22 @@ def training(prof):
 @app.route("/list_prof/<type_of_list>")
 def list_prof(type_of_list):
     return render_template("list_prof.html", type_of_list=type_of_list)
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def answer():
+    page_title = "Анкета"
+    data = {
+        "Фамилия": "Watny",
+        "Имя": "Mark",
+        "Образование": "Выше среднего",
+        "Профессия": "Штурман марсохода",
+        "Пол": "male",
+        "Мотивация": "Всегда мечтал застрять на Марсе!",
+        "Готовы остаться на Марсе": "True"}
+    keys_and_data = [{"title": i, "data": data[i]} for i in data.keys()]
+    return render_template("answer.html", keys_and_data=keys_and_data, page_title=page_title)
 
 
 if __name__ == '__main__':
