2419dd3c284e7d67adf95103fb6c333e09f0b0db
vlad <v1adryazanov@yandex.com>
2024-02-08 15:21:29 +0300

    версия 6

--- app.py
+++ app.py
@@ -45,5 +45,11 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route("/distribution")
+def distribution():
+    list_of_astronauts = ["Ритни Скотт", "Эдди Уир", "Марк Уотни"]
+    return render_template("distribution.html", list_of_astronauts=list_of_astronauts)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
